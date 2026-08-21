64632a647a21d012c267448f2b01f5080eed31fd
kangdims <dimas.879@gmail.com>
2026-08-21 16:33:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import glob
-import json
 from openai import OpenAI
 
 # ---------------------------------------------------------
